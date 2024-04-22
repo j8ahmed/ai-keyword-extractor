8bf827669bbfb8d0f401c18eb9f8a33d4b4972e8
Jamal Ahmed <j8ahmed@gmail.com>
2024-04-21 21:27:14 -0400

    Add core components and features

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,86 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Container, Box } from '@chakra-ui/react';
+import { 
+    Footer,
+    Header,
+    KeywordsModal,
+    TextInput,
+} from './components';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const [keywords, setKeywords] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const extractKeywords = async (text) => {
+        console.log(text)
+        setLoading(true);
+        setIsOpen(true);
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+            },
+            body: JSON.stringify({
+                messages: [
+                    {
+                        role: 'user',
+                        content:
+                            'Extract keywords from this text. Make the first letter of each word uppercase and separate with commas\n\n' +
+                            text,
+                    },
+                ],
+                model: 'gpt-3.5-turbo',
+                prompt:
+                    'Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n' +
+                    text +
+                    '',
+                temperature: 0.5,
+                max_tokens: 60,
+                top_p: 1.0,
+                frequency_penalty: 0.8,
+                presence_penalty: 0.0,
+            }),
+        };
+
+        //try {
+        //    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+        //    const json = await response.json();
+        //    console.log(json.choices[0].text.trim());
+        //    setKeywords(json.choices[0].text.trim());
+        //    setLoading(false);
+        //} catch (error) {
+        //    console.error(error);
+        //}
+        setTimeout(() => {
+            setKeywords("Demo: JavaScript, Data, OpenAI, API");
+            setLoading(false);
+        }, 5000)
+    }
+
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+
+    return (
+        <Box bg="blue.600" color="white" height="100vh" paddingTop="130">
+            <Container centerContent>
+                <Header />
+
+                <TextInput extractKeywords={extractKeywords} />
+
+                <Footer />
+            </Container>
+
+            <KeywordsModal
+                keywords={keywords}
+                loading={loading}
+                isOpen={isOpen}
+                closeModal={closeModal}
+            />
+            </Box>
+    )
 }
-
-export default App
---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
+export { default as TextInput } from "./TextInput";
+export { default as KeywordsModal } from "./KeywordsModal";
